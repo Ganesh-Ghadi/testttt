850a4ad8b8bb1a4a39745f6c6c8e82970b45c745
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-01-01 23:36:08 +0530

    setup fixed

--- frontend/src/customComponents/SIdebar/Sidebar.jsx
+++ frontend/src/customComponents/SIdebar/Sidebar.jsx
@@ -4,6 +4,7 @@ import { IoLogoSlack } from 'react-icons/io';
 import { NavLink } from 'react-router-dom';
 import { IoIosArrowDown } from 'react-icons/io';
 import Navbar from '../Navbar/Navbar';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -61,6 +62,312 @@ const Sidebar = () => {
       path: '/contact',
       logo: <IoLogoSlack />,
     },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+      logo: <IoLogoSlack />,
+      children: [
+        {
+          name: 'Registration',
+          path: '/registration',
+          logo: <IoLogoSlack />,
+        },
+        {
+          name: 'All Registration',
+          path: '/registrationlist',
+          logo: <IoLogoSlack />,
+        },
+      ],
+    },
   ];
 
   // Function to toggle children visibility (close previous and open current)
@@ -70,19 +377,24 @@ const Sidebar = () => {
 
   return (
     <>
-      <div
-        className={`${
-          isSidebarOpen ? 'w-72' : 'w-16'
-        } hidden md:block duration-300 px-4 pt-3.5 relative h-screen dark:bg-gray-800 bg-slate-100`}
+      <ScrollArea
+        className={`hidden md:block duration-300 min-h-screen ${
+          isSidebarOpen ? 'w-96' : 'w-24'
+        }`}
       >
-        <button onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-          <IoIosArrowDropleft
-            className={`absolute rounded-full cursor-pointer -right-3 top-32 w-7 h-7 border-2 border-black dark:border-0 ${
-              !isSidebarOpen && 'rotate-180'
-            }`}
-          />
-        </button>
-        {/* <div className="flex gap-x-4 items-center">
+        <div
+          className={`${
+            isSidebarOpen ? 'w-72' : 'w-16'
+          } hidden md:block duration-300 px-4 pt-3.5 relative min-h-screen   dark:bg-gray-800 bg-slate-100`}
+        >
+          <button onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <IoIosArrowDropleft
+              className={`absolute rounded-full cursor-pointer -right-3 top-32 w-7 h-7 border-2 border-black dark:border-0 ${
+                !isSidebarOpen && 'rotate-180'
+              }`}
+            />
+          </button>
+          {/* <div className="flex gap-x-4 items-center">
         <p className="text-4xl p-1 text-black">
           <IoLogoSlack />
         </p>
@@ -94,72 +406,73 @@ const Sidebar = () => {
           Designer
         </p>
       </div> */}
-        <ul className="mt-10">
-          {items.map((item, index) => {
-            return (
-              <div key={index}>
-                {/* Parent item */}
-                <NavLink
-                  className={({ isActive }) =>
-                    `flex text-sm my-2 p-1 rounded hover:bg-light-white items-center ${
-                      isActive
-                        ? 'bg-gray-300 dark:bg-gray-600'
-                        : 'border-b-2 border-gray-200 dark:border-gray-600'
-                    } `
-                  }
-                  to={item.path || '#'}
-                  onClick={() => item.children && toggleChildren(item.name)} // Toggle children visibility on click
-                >
-                  <p className="text-xl">{item.logo}</p>
-                  <div
-                    className={`w-full px-2 flex justify-between items-center ${
-                      !isSidebarOpen && 'opacity-0 invisible'
-                    }`}
+          <ul className="pb-32 ">
+            {/* mt-10 about */}
+            {items.map((item, index) => {
+              return (
+                <div key={index}>
+                  {/* Parent item */}
+                  <NavLink
+                    className={({ isActive }) =>
+                      `flex text-sm my-2 p-1 rounded hover:bg-light-white items-center ${
+                        isActive
+                          ? 'bg-gray-300 dark:bg-gray-600'
+                          : 'border-b-2 border-gray-200 dark:border-gray-600'
+                      } `
+                    }
+                    to={item.path || '#'}
+                    onClick={() => item.children && toggleChildren(item.name)} // Toggle children visibility on click
                   >
-                    <p className="">{item.name}</p>
-                    <p className="">
-                      <IoIosArrowDown
-                        className={`${
-                          item.children &&
-                          activeParent === item.name &&
-                          'rotate-180'
-                        }`}
-                      />
-                    </p>
-                  </div>
-                </NavLink>
-                {/* Render children if the parent item has children and it's the active one */}
-                {item.children && activeParent === item.name && (
-                  <div>
-                    {item.children.map((child, idx) => (
-                      <NavLink
-                        key={idx}
-                        className={`pl-1 w-full py-1 my-2 gap-2 flex items-center text-sm text-gray-600 dark:text-gray-300 hover:bg-gray-600 ${
-                          isSidebarOpen && 'pl-8'
-                        } `}
-                        to={child.path}
-                      >
-                        <p className="text-xl">{child.logo}</p>
-                        <p
+                    <p className="text-xl">{item.logo}</p>
+                    <div
+                      className={`w-full px-2 flex justify-between items-center ${
+                        !isSidebarOpen && 'opacity-0 invisible'
+                      }`}
+                    >
+                      <p className="">{item.name}</p>
+                      <p className="">
+                        <IoIosArrowDown
                           className={`${
-                            !isSidebarOpen && 'hidden'
-                          } origin-left duration-300 text-sm`}
+                            item.children &&
+                            activeParent === item.name &&
+                            'rotate-180'
+                          }`}
+                        />
+                      </p>
+                    </div>
+                  </NavLink>
+                  {/* Render children if the parent item has children and it's the active one */}
+                  {item.children && activeParent === item.name && (
+                    <div>
+                      {item.children.map((child, idx) => (
+                        <NavLink
+                          key={idx}
+                          className={`pl-1 w-full py-1 my-2 gap-2 flex items-center text-sm text-gray-600 dark:text-gray-300 hover:bg-gray-600 ${
+                            isSidebarOpen && 'pl-8'
+                          } `}
+                          to={child.path}
                         >
-                          {child.name}
-                        </p>
-                      </NavLink>
-                    ))}
-                    <p className="border-b-2 border-gray-200"></p>
-                  </div>
-                )}
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+                          <p className="text-xl">{child.logo}</p>
+                          <p
+                            className={`${
+                              !isSidebarOpen && 'hidden'
+                            } origin-left duration-300 text-sm`}
+                          >
+                            {child.name}
+                          </p>
+                        </NavLink>
+                      ))}
+                      <p className="border-b-2 border-gray-200"></p>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </ScrollArea>
     </>
   );
 };
 
 export default Sidebar;
-//hhh
--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,40 +1,41 @@
-import React, { useEffect, useRef, useState } from "react";
-import Sidebar from "../customComponents/SIdebar/Sidebar";
-import { Outlet, useNavigate } from "react-router-dom";
-import MobileSidebar from "../customComponents/SIdebar/MobileSidebar";
-import { FaRegMoon } from "react-icons/fa";
-import { LuSunMedium } from "react-icons/lu";
-import logo from "../assets/react.svg";
-import { TbLogout2 } from "react-icons/tb";
-import { toast } from "react-toastify";
-import axios from "axios";
-import Navbar from "../customComponents/Navbar/Navbar";
+import React, { useEffect, useRef, useState } from 'react';
+import Sidebar from '../customComponents/SIdebar/Sidebar';
+import { Outlet, useNavigate } from 'react-router-dom';
+import MobileSidebar from '../customComponents/SIdebar/MobileSidebar';
+import { FaRegMoon } from 'react-icons/fa';
+import { LuSunMedium } from 'react-icons/lu';
+import logo from '../assets/react.svg';
+import { TbLogout2 } from 'react-icons/tb';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import Navbar from '../customComponents/Navbar/Navbar';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const MainLayout = ({ toggleTheme, darkMode }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(localStorage.getItem('user'));
   const token = user.token;
   const modalRef = useRef(null); // Create a ref for the sidebar
   const navigate = useNavigate();
   const logout = async () => {
     try {
       // Make API request with axios
-      const response = await axios.get("http://127.0.0.1:8000/api/logout", {
+      const response = await axios.get('http://127.0.0.1:8000/api/logout', {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`, // Include the Bearer token
         },
       });
-      toast.success("Logged-out successfully");
-      localStorage.removeItem("user");
-      navigate("/login");
+      toast.success('Logged-out successfully');
+      localStorage.removeItem('user');
+      navigate('/login');
     } catch (error) {
       if (error.response) {
-        toast.error("logout failed: " + error.response.data); // Customize error message
+        toast.error('logout failed: ' + error.response.data); // Customize error message
       } else if (error.request) {
-        toast.error("No response from server. Please try again later.");
+        toast.error('No response from server. Please try again later.');
       } else {
-        toast.error("An error occurred while logout.");
+        toast.error('An error occurred while logout.');
       }
     }
   };
@@ -47,30 +48,47 @@ const MainLayout = ({ toggleTheme, darkMode }) => {
     };
 
     if (isModalOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutside);
     } else {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     }
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [isModalOpen]);
 
   return (
     <>
-      <Navbar />
-      <MobileSidebar
-        logout={logout}
-        setIsModalOpen={setIsModalOpen}
-        isModalOpen={isModalOpen}
-        darkMode={darkMode}
-        toggleTheme={toggleTheme}
-      />
-      <div className=" bg-gray-50 min-h-screen dark:bg-gray-900 flex md:flex-row flex-col ">
-        <Sidebar />
-        <div className="p-7 flex-1 h-screen  overflow-y-auto">
+      {/* <div className="w-full">
+        <Navbar />
+        <div className="w-full p-7">
           <Outlet />
+        </div>
+      </div> */}
+      {/* <div className="w-full flex  flex-col">
+        <Navbar />
+        <div className="w-full flex overflow-hidden">
+          <div className="h-screen">
+            <Sidebar />
+          </div>
+          <div className="w-full p-7 flex flex-col overflow-y-auto">
+            <Outlet />
+          </div>
+        </div>
+      </div> */}
+
+      <div className="flex flex-col h-screen">
+        <Navbar />
+
+        <div className="flex flex-1 overflow-hidden">
+          {/* <div className="min-h-screen auo"> */}
+            <Sidebar />
+          {/* </div> */}
+
+          <div className="w-full p-7 overflow-y-auto">
+            <Outlet />
+          </div>
         </div>
       </div>
     </>
